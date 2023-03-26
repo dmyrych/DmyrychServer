3677b6c825561be29ca8d6da0d30bbf25ea79897
ваше имя <dmyrych17@gmail.com>
2023-03-26 20:59:10 +0300

    Added some sort of connection to client. Still got a ton of work to do

--- DmyrychServer/Program.cs
+++ DmyrychServer/Program.cs
@@ -1,8 +1,52 @@
 ﻿using System;
+using System.Data;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
+
+public class Database
+{
+    private MySqlConnection connection;
+
+    public Database(string connectionString)
+    {
+        connection = new MySqlConnection(connectionString + ";database=ServerDb");
+    }
+
+    public void Open()
+    {
+        if (connection.State != ConnectionState.Open)
+        {
+            connection.Open();
+        }
+    }
+
+    public void Close()
+    {
+        if (connection.State != ConnectionState.Closed)
+        {
+            connection.Close();
+        }
+    }
+
+    public DataTable GetUsers()
+    {
+        Open();
+        DataTable dataTable = new DataTable();
+        using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM users", connection))
+        {
+            using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+            {
+                adapter.Fill(dataTable);
+            }
+        }
+        Close();
+        return dataTable;
+    }
+}
 public class Server
 {
     private Socket serverSocket;
@@ -18,39 +62,61 @@ public class Server
             serverSocket.Listen(1);
             Console.WriteLine("Server started... Waiting for connections...");
 
-            //Waiting for connections
-            Socket clientSocket = serverSocket.Accept();
-            Console.WriteLine("Client connected: {0}", clientSocket.RemoteEndPoint);
-
-            //Read client data
-            while (true)
+            Task.Run(async () =>
             {
-                int bytesRead = clientSocket.Receive(buffer);
-                if(bytesRead == 0)
+                while (true)
                 {
-                    Console.WriteLine("Client disconnected and sucks penises");
-                    break;
-                }
+                    Socket clientSocket = null;
+                    try
+                    {
+                        //Waiting for connections
+                        clientSocket = await serverSocket.AcceptAsync();
+                        Console.WriteLine("Client connected: {0}", clientSocket.RemoteEndPoint);
 
-                string message = Encoding.Unicode.GetString(buffer, 0, bytesRead);
-                Console.WriteLine("Recieved message: ", message);
-            }
-            //Closing connection if client is gay
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
-            serverSocket.Close();
+                        //Read client data
+                        while (true)
+                        {
+                            int bytesRead = clientSocket.Receive(buffer);
+                            if (bytesRead == 0)
+                            {
+                                Console.WriteLine("Client disconnected: {0}", clientSocket.RemoteEndPoint);
+                                clientSocket.Shutdown(SocketShutdown.Both);
+                                clientSocket.Close();
+                                break;
+                            }
+
+                            string message = Encoding.Unicode.GetString(buffer, 0, bytesRead);
+                            Console.WriteLine("Received message from {0}: {1}", clientSocket.RemoteEndPoint, message);
+                        }
+                    }
+                    catch (SocketException e)
+                    {
+                        //Console.WriteLine("Socket exception: {0}", e);
+                        if (clientSocket != null)
+                        {
+                            Console.WriteLine("Client disconnected: {0}", clientSocket.RemoteEndPoint);
+                            clientSocket.Shutdown(SocketShutdown.Both);
+                            clientSocket.Close();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: {0}", e);
+                    }
+                }
+            });
         }
         catch (Exception e)
         {
-            Console.WriteLine("Exception: ", e);
+            Console.WriteLine("Exception: {0}", e.ToString());
         }
-        
     }
 
     static void Main(string[] args)
     {
         Server server = new Server();
-        server.Start(3456);
+        server.Start(29875);
+        Console.ReadLine();
     }
 }
 
